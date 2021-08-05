1c981e0301c09611d28826d0ae063cae6463eb3b
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2021-08-05 13:07:04 +0300

    Выполнены пункты 2 и 3 ТЗ. Создан фрагмент "ListNotesFragment" для вывода списка класса Notepad. Фрагмент "ListNotesFragment" встоен в "MainActivity" и отображает список созданных заметок.
    Ссылк на репозиторий проекта:
    https://github.com/AVKhakhalin/NotepadOnFragments

--- app/src/main/java/ru/geekbrains/lessions2345/notepadonfragments/ui/MainActivity.java
+++ app/src/main/java/ru/geekbrains/lessions2345/notepadonfragments/ui/MainActivity.java
@@ -1,22 +1,63 @@
 package ru.geekbrains.lessions2345.notepadonfragments.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import ru.geekbrains.lessions2345.notepadonfragments.R;
 import ru.geekbrains.lessions2345.notepadonfragments.logic.Notepad;
+import ru.geekbrains.lessions2345.notepadonfragments.ui.fragments.ListNotesFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String KEY_NOTEPAD = "Notepad";
+    Notepad notepad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Notepad notepad = new Notepad();
+        // Инициализация класса Notepad
+        notepad = new Notepad();
+        notepad.add("Первая", "Первый элемент");
+        notepad.add("Вторая", "Второй элемент");
+        notepad.add("Третья", "Третий элемент");
+        notepad.add("Четвёртая", "Четвёртый элемент");
+        notepad.add("Пятая", "Пятый элемент");
 
+        Toast.makeText(this, notepad.getName(1), Toast.LENGTH_SHORT).show();
 
+/*
+        // добавляем фрагмент
+        ListNotesFragment listNotesFragment = new ListNotesFragment();
+        // Передача класса notepad во фрагмент ListNotesFragment
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(KEY_NOTEPAD, notepad);
+        listNotesFragment.setArguments(bundle);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.list_container, listNotesFragment)
+                .commit();
+*/
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.list_container, ListNotesFragment.newInstance(notepad))
+                .commit();
+    }
+
+    // Метод для считывания класса во фрагменты
+    public Notepad getNotepad()
+    {
+        return notepad;
+    }
+
+    // Метод для изменения класса notepad через фрагменты
+    public void setNotepad(Notepad notepad)
+    {
+        this.notepad = notepad;
     }
 }
--- app/src/main/java/ru/geekbrains/lessions2345/notepadonfragments/ui/fragments/ListNotesFragment.java
+++ app/src/main/java/ru/geekbrains/lessions2345/notepadonfragments/ui/fragments/ListNotesFragment.java
@@ -1,7 +1,89 @@
 package ru.geekbrains.lessions2345.notepadonfragments.ui.fragments;
 
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.fragment.app.Fragment;
+
+import ru.geekbrains.lessions2345.notepadonfragments.R;
+import ru.geekbrains.lessions2345.notepadonfragments.logic.Notepad;
+import ru.geekbrains.lessions2345.notepadonfragments.ui.MainActivity;
 
 public class ListNotesFragment extends Fragment {
 
+    Notepad notepad;
+
+    public static ListNotesFragment newInstance(Notepad notepad)
+    {
+        ListNotesFragment listNotesFragment = new ListNotesFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(MainActivity.KEY_NOTEPAD, notepad);
+        listNotesFragment.setArguments(bundle);
+        return listNotesFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Восстановление класса notepad в текущем фрагменте
+        if (getArguments() != null)
+        {
+            this.notepad = getArguments().getParcelable(MainActivity.KEY_NOTEPAD);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        LinearLayout linearLayout = (LinearLayout) view;
+
+        // Считывание класса notepad
+
+/*
+        if (savedInstanceState != null)
+        {
+            notepad = this.getArguments().getParcelable(MainActivity.KEY_NOTEPAD);
+            textView.setText(notepad.getName(1));
+        }
+        else
+        {
+            textView.setText("savedInstanceState is empty");
+        }
+*/
+
+/*        // Получение класса notepad из MainActivity
+        MainActivity mainActivity = (MainActivity) getActivity();
+        notepad = mainActivity.getNotepad();*/
+
+        // Установка значения текстового поля
+        for (int i = 1; i <= notepad.getNumberElements(); i++)
+        {
+            TextView textView = new TextView(getContext());
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
+            {
+                textView.setText(notepad.getDescription(i));
+            }
+            else
+            {
+                textView.setText(notepad.getName(i));
+            }
+            // Форматирование текстового поля
+            textView.setTextSize(20);
+            linearLayout.addView(textView);
+            textView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v){
+                    // Загрузка второго фрагмента
+                }
+            });
+        }
+        return view;
+    }
 }
