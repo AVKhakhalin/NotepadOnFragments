8d3bd6fa6a94066c23a8bcec95cada561475444a
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2021-08-31 15:49:42 +0300

    Предпринята неудачная попытка переписать класс "CardsSourceFirebaseImpl" на Котлин. Переписал на Котлин, ошибок при компиляции не обнаружено, но из "Firebase" не загружаются теперь заметки. Для обеспечения работоспособности программы сохранил созданный класс на котлине под именем "CardsSourceFirebaseImplKotlinNotWork" и вернул рабочий java-класс "CardsSourceFirebaseImpl".
    Репозиторий хранится на сайте github.com:
    https://github.com/AVKhakhalin/NotepadOnFragments

---
+++ app/src/main/java/ru/geekbrains/lessions2345/notepadonfragments_2/logic/CardsSourceFirebaseImplKotlinNotWork.kt
@@ -0,0 +1,142 @@
+package ru.geekbrains.lessions2345.notepadonfragments_2.logic
+
+import android.util.Log
+import androidx.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.*
+import com.google.firebase.firestore.core.UserData
+import java.util.concurrent.Executor
+
+class CardsSourceFirebaseImplKotlinNotWork : CardSource {
+
+    private val CARDS_COLLECTION : String = "cards"
+    private val TAG : String = "[CardsSourceFirebaseImpl]"
+    private var cardData : CardNote? = null
+
+    // База данных Firebase
+    private val store : FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    // Коллекция документов
+    private val collection : CollectionReference = store.collection(CARDS_COLLECTION)
+
+    // Загружаемый список карточек
+    private var cardsData : ArrayList<CardNote> = ArrayList<CardNote>()
+
+    public fun init(cardsSourceResponse : CardSourceResponse) : CardSource {
+        // Получить всю коллекцию отсортированную по полю "год"
+        collection.orderBy(CardNoteMapping.Fields.YEAR, Query.Direction.ASCENDING).get(Source.SERVER)
+            .addOnCompleteListener(OnCompleteListener<QuerySnapshot>() {
+                // При удачном считывании данных загрузим список карточек
+                @Override
+                fun onComplete(@NonNull task : Task<QuerySnapshot> ) {
+                    if (task.isSuccessful()) {
+//                        cardsData = ArrayList<CardNote>()
+                        var result = task.getResult()
+                        if (result != null) {
+                            for (document: QueryDocumentSnapshot in result) {
+                                var doc : HashMap<String, Any> = document.getData() as HashMap<String, Any>
+                                var id : String = document.id
+//                                var cardData : CardNote = CardNoteMapping.Fields.toCardNote(id, doc)
+                                cardData = CardNoteMapping.Fields.toCardNote(id, doc)
+//                                (cardsData as ArrayList<CardNote>).add(cardData)
+                                cardsData.add(cardData as CardNote)
+                            }
+                        }
+                        Log.d(TAG, "success ${cardsData.size} qnt")
+                        cardsSourceResponse.initialized(this)
+                    } else {
+                        Log.d(TAG, "get failed with ", task.exception)
+                    }
+                }
+
+            })
+            .addOnFailureListener(OnFailureListener {
+                @Override
+                fun onFailure(@NonNull e : Exception) {
+                    Log.d(TAG, "get failed with ", e)
+                }
+            })
+        return this
+    }
+
+    public fun getCardNoteFirebase(position : Int) : CardNote {
+        return cardsData.get(position)
+    }
+
+    public override fun size() : Int {
+        if (cardsData == null) {
+            return 0
+        }
+        return cardsData.size
+    }
+
+    public fun deleteCardNoteFirebase(position : Int) {
+        // Удалить документ с определенном идентификатором
+        if (cardsData != null) {
+            collection.document(cardsData.get(position).getId()).delete()
+            cardsData.removeAt(position)
+        }
+    }
+
+    public fun updateCardNoteFirebase(position : Int, cardData : CardNote) {
+        var id : String = cardData.getId();
+        // Изменить документ по идентификатору
+        collection.document(id).set(CardNoteMapping.Fields.toDocument(cardData));
+    }
+
+    public fun addCardNoteFirebase(cardData : CardNote) {
+        // Добавить документ
+        collection.add(CardNoteMapping.Fields.toDocument(cardData)).addOnSuccessListener(OnSuccessListener<DocumentReference>() {
+            @Override
+            fun onSuccess(documentReference : DocumentReference) {
+                cardData.setId(documentReference.getId())
+            }
+        })
+    }
+
+    public fun clearCardNoteFirebase() {
+        for (cardData : CardNote in cardsData) {
+            collection.document(cardData.getId()).delete()
+        }
+        cardsData = ArrayList<CardNote>()
+    }
+
+    override fun getCardNote(position: Int): CardNote {
+        return cardsData.get(position)
+    }
+
+    override fun getListNotes(): ListNotes {
+        TODO("Not yet implemented")
+    }
+
+    override fun addCardNote() {
+        TODO("Not yet implemented")
+    }
+
+    override fun addCardNote(cardNote: CardNote?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setCardNote(position: Int, cardNote: CardNote?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setCardNote(position: Int, name: String?, description: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setCardNote(position: Int, text: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setCardNote(position: Int, year: Int, month: Int, day: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeCardNote(position: Int) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/ru/geekbrains/lessions2345/notepadonfragments_2/logic/CardsSourceFirebaseImplOriginalJava.java
@@ -0,0 +1,159 @@
+package ru.geekbrains.lessions2345.notepadonfragments_2.logic;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Source;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CardsSourceFirebaseImplOriginalJava implements CardSource {
+
+    private static final String CARDS_COLLECTION = "cards";
+    private static final String TAG = "[CardsSourceFirebaseImpl]";
+    CardNote cardData;
+
+    // База данных Firestore
+    private FirebaseFirestore store = FirebaseFirestore.getInstance();
+
+    // Коллекция документов
+    private CollectionReference collection = store.collection(CARDS_COLLECTION);
+
+    // Загружаемый список карточек
+    private List<CardNote> cardsData = new ArrayList<CardNote>();
+
+    public CardSource init(final CardSourceResponse cardsSourceResponse) {
+        // Получить всю коллекцию отсортированную по полю "год"
+        collection.orderBy(CardNoteMapping.Fields.YEAR, Query.Direction.ASCENDING).get(Source.SERVER)
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    // При удачном считывании данных загрузим список карточек
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            cardsData = new ArrayList<CardNote>();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Map<String, Object> doc = document.getData();
+                                String id = document.getId();
+//                            CardNote cardData = CardNoteMapping.toCardNote(id, doc);
+//                                cardData = CardNoteMapping.toCardNote(id, doc);
+                                cardData = CardNoteMapping.Fields.toCardNote(id, (HashMap<String, Object>) doc);
+                                cardsData.add(cardData);
+                            }
+                            Log.d(TAG, "success " + cardsData.size() + " qnt");
+                            cardsSourceResponse.initialized(CardsSourceFirebaseImplOriginalJava.this);
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "get failed with ", e);
+                    }
+                });
+        return this;
+    }
+
+    public CardNote getCardNoteFirebase(int position) {
+        return cardsData.get(position);
+    }
+
+    @Override
+    public int size() {
+        if (cardsData == null) {
+            return 0;
+        }
+        return cardsData.size();
+    }
+
+    public void deleteCardNoteFirebase(int position) {
+        // Удалить документ с определенном идентификатором
+        collection.document(cardsData.get(position).getId()).delete();
+        cardsData.remove(position);
+    }
+
+    public void updateCardNoteFirebase(int position, CardNote cardData) {
+        String id = cardData.getId();
+        // Изменить документ по идентификатору
+//        collection.document(id).set(CardNoteMapping.toDocument(cardData));
+        collection.document(id).set(CardNoteMapping.Fields.toDocument(cardData));
+    }
+
+    public void addCardNoteFirebase(final CardNote cardData) {
+        // Добавить документ
+//        collection.add(CardNoteMapping.toDocument(cardData)).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        collection.add(CardNoteMapping.Fields.toDocument(cardData)).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                cardData.setId(documentReference.getId());
+            }
+        });
+    }
+
+    public void clearCardNoteFirebase() {
+        for (CardNote cardData : cardsData) {
+            collection.document(cardData.getId()).delete();
+        }
+        cardsData = new ArrayList<CardNote>();
+    }
+
+    @Override
+    public CardNote getCardNote(int position) {
+        return cardsData.get(position);
+    }
+
+    @Override
+    public ListNotes getListNotes() {
+        return null;
+    }
+
+    @Override
+    public void addCardNote() {
+
+    }
+
+    @Override
+    public void addCardNote(CardNote cardNote) {
+
+    }
+
+    @Override
+    public void setCardNote(int position, CardNote cardNote) {
+
+    }
+
+    @Override
+    public void setCardNote(int position, String name, String description) {
+
+    }
+
+    @Override
+    public void setCardNote(int position, String text) {
+
+    }
+
+    @Override
+    public void setCardNote(int position, int year, int month, int day) {
+
+    }
+
+    @Override
+    public void removeCardNote(int position) {
+
+    }
+}
