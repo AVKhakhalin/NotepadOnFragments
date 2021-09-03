b763689d347669ad57eefc634561af5a5d80a3d0
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2021-09-03 13:29:42 +0300

    Исправлена версия Котлина в "gradle"-файле.
    Предпринята неудачная попытка переписать интерфейс "ListNotesFragmentOnClickListener" на Котлин.
    
    Приложение запускается и работает и с переписанными модулями на Котлин.
    
    Создан apk-файл.
    Место размещения apk-файла:
    app\
    
    Репозиторий хранится на сайте github.com:
    https://github.com/AVKhakhalin/NotepadOnFragments

---
+++ app/src/main/java/ru/geekbrains/lessions2345/notepadonfragments_2/ui/fragments/ListNotesFragmentOnClickListenerNotWork.kt
@@ -0,0 +1,7 @@
+package ru.geekbrains.lessions2345.notepadonfragments_2.ui.fragments
+
+import android.view.View
+
+interface ListNotesFragmentOnClickListenerNotWork {
+    fun onClick(view : View, position : Int)
+}
